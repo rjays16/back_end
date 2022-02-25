08b5f7eb80c0debe0c639b4e55026d26d4442c88
allan81 <rjaycondiman81@gmail.com>
2022-02-25 11:15:03 +0800

    initial push

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,5 +1,9 @@
 require('./bootstrap');
-
+import Vue from 'vue';
 window.Vue = require('vue');
 
 Vue.component('HelloWorld', require('../../../frontend/src/components/HelloWorld.vue').default);
+
+const app = new Vue({
+    el: '#app',
+});
